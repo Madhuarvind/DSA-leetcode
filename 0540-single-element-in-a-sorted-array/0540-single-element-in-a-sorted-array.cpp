6a8eb72e25beb3256ea2a5ce190a6b94db852ff5
Madhuaravind P <166808473+Madhuarvind@users.noreply.github.com>
2026-02-12 12:50:43 +0530

    Time: 0 ms (100%), Space: 26.2 MB (67.86%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int n = nums.size(); // size of the array.
-
-        // Edge cases:
-        if (n == 1) return nums[0];
-        if (nums[0] != nums[1]) return nums[0];
-        if (nums[n - 1] != nums[n - 2]) return nums[n - 1];
-
-        int low = 1, high = n - 2;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            // if nums[mid] is the single element:
-            if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]) {
+        if(nums.size()==1) return nums[0];
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[nums.size()-1]!=nums[nums.size()-2]) return nums[nums.size()-1];
+        int low=1,high=nums.size()-2;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]!=nums[mid-1]&&nums[mid]!=nums[mid+1]){
                 return nums[mid];
             }
-
-            // we are in the left:
-            if ((mid % 2 == 1 && nums[mid] == nums[mid - 1])
-                || (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
-                // eliminate the left half:
-                low = mid + 1;
-            }
-            // we are in the right:
-            else {
-                // eliminate the right half:
-                high = mid - 1;
+            if((mid % 2 == 1 && nums[mid]==nums[mid-1])||
+            (mid%2==0&&nums[mid]==nums[mid+1])){
+                low=mid+1;
+            } else {
+                high=mid-1;
             }
         }
-
-        // dummy return statement:
         return -1;
     }
 };
